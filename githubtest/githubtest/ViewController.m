ecec86861e8dfd2b91de2359fab4320cc9c60d47
glz <glz@163.com>
2015-08-25 16:40:51 +0800

    add some log

--- githubtest/githubtest/ViewController.m
+++ githubtest/githubtest/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Hello friends I am comeing");
+    NSLog(@"Thanks for you comeing to look at it ");
 }
 
 - (void)didReceiveMemoryWarning {
